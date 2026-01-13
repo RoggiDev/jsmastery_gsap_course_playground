c3526bbcb136906aebdf9e456ac69a4f71d4b1c9
RoggiDev <yael.rodriguez.g@outlook.es>
2026-01-12 22:54:39 -0600

    first commit

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,21 @@
+gsap.to(".card", {
+  opacity: 1,
+  scale: 1,
+  duration: 1,
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -20,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    });
+
+    // Pulsing glow animation
+    gsap.to(".card", {
+      boxShadow: "0 20px 80px rgba(29, 289, 161, 0.5",
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+    });
+  },
+});
